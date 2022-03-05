40903827b567faa3562256e6d330892a15ef92b4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-03-05 12:12:02 +0900

    remove redundant space

--- src/test/kotlin/FooTest.kt
+++ src/test/kotlin/FooTest.kt
@@ -4,6 +4,6 @@ import io.kotest.matchers.shouldBe
 class FooTest : StringSpec({
     "failing test" {
         val foo = Foo()
-        foo.method1() shouldBe  1
+        foo.method1() shouldBe 1
     }
 })
